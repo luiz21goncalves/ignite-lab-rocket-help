fda4c7b89ba6ce0bdd868c5f99153216e8b1e248
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-07-19 10:56:44 -0300

    feat: add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { Text } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading'; 
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Text>Hello</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
