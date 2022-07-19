caea1818206d3bfc9a06e8e2a0f34d957f71c258
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-07-19 14:21:01 -0300

    feat: add input

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,36 @@
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from 'phosphor-react-native'
+
+import { Input } from "../../components/Input";
+import LogoPrimary from '../../assets/logo_primary.svg'
+
+export function SignIn() {
+  const { colors } = useTheme()
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <LogoPrimary />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6} >
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        placeholder='E-mail'
+        mb={4}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        } 
+      />
+
+      <Input
+        placeholder='Senha'
+        mb={4}
+        InputLeftElement={
+          <Icon as={<Key color={colors.gray[300]} />} ml={4} />
+        }
+        secureTextEntry
+      />
+    </VStack>
+  )
+}
