4dfcc27b75f1b2d540bce5d02cd0647a1ff20d9a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-07-20 15:24:19 -0300

    fix: loading color

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,7 @@ import { Center, Spinner } from "native-base";
 export function Loading() {
   return (
     <Center flex={1} bg="gray.700">
-      <Spinner size="lg"color="secondary.700" />
+      <Spinner size="lg"color="primary.700" />
     </Center>
   )
 }
